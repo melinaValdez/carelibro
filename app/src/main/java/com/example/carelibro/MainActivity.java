7310c5dcb01e764a6655c162a238ea6a2cf35bb0
Melina <melinamonserrat2010@hotmail.com>
2019-11-13 05:50:52 -0600

    Some changes

--- app/src/main/java/com/example/carelibro/MainActivity.java
+++ app/src/main/java/com/example/carelibro/MainActivity.java
@@ -30,6 +30,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
@@ -232,13 +233,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void displayAllUsersPosts(){
+        Query orderedPostsReference = postsReference.orderByChild("time");
         FirebaseRecyclerAdapter<Posts, PostsViewHolder> firebaseRecyclerAdapter =
                 new FirebaseRecyclerAdapter<Posts, PostsViewHolder>
                         (
                                 Posts.class,
                                 R.layout.all_posts_layout,
                                 PostsViewHolder.class,
-                                postsReference
+                                orderedPostsReference
                         ) {
                     @Override
                     protected void populateViewHolder(final PostsViewHolder postsViewHolder, Posts model, int position) {
