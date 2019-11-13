04f19a54c866edf1dba99e1671c83aeea3ae1c52
Andrey <andrey20ag95@gmail.com>
2019-11-13 05:58:04 -0600

    amigos profile

---
+++ app/src/main/java/com/example/carelibro/PersonProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.carelibro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PersonProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person_profile);
+    }
+}
